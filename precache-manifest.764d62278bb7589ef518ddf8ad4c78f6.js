04759dce2a40a4d1d2dd589d927c1c4a1feec77d
Dragos Dehelean <dragos.dehelean@selenis.ro>
2019-02-20 16:18:38 +0200

    Updates

---
+++ precache-manifest.764d62278bb7589ef518ddf8ad4c78f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "972696a550620d4ab417",
+    "url": "/React-Redux-Scoreboard-App/static/js/runtime~main.972696a5.js"
+  },
+  {
+    "revision": "1f54ed5e696f633d4b46",
+    "url": "/React-Redux-Scoreboard-App/static/js/main.1f54ed5e.chunk.js"
+  },
+  {
+    "revision": "4bdd36901fa687ed05e7",
+    "url": "/React-Redux-Scoreboard-App/static/js/2.4bdd3690.chunk.js"
+  },
+  {
+    "revision": "1f54ed5e696f633d4b46",
+    "url": "/React-Redux-Scoreboard-App/static/css/main.ac50a0de.chunk.css"
+  },
+  {
+    "revision": "e07aba8d061fffdfdbf000821df82038",
+    "url": "/React-Redux-Scoreboard-App/index.html"
+  }
+];
